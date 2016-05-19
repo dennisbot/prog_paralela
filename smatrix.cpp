a2b497ebf69345f0a9e551a27a9c6197c386cfa2
Dennis Huillca Portilla <dennisbot@gmail.com>
2016-05-19 18:11:52 -0500

    Add files via upload
    cambios, se añadió el código del 6 nested loop

--- smatrix.cpp
+++ smatrix.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
-#define N 10000
+#define N 1500
+// CLS based at intel Core i7 Processor Speed: 2.9 GHz
+#define CLS 2*32
+#define SM (CLS / sizeof(double) < N ? CLS / sizeof(double) : N)
+
 using namespace std;
 
 int i, j, k;
-int mul1[N][N], mul2[N][N], res[N][N], temp[N][N], sum;
+double mul1[N][N], mul2[N][N], res[N][N], temp[N][N], sum;
 
 void resetMatrix() {
-	sum = 0;
-	memset(res, sizeof(int), 0);
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++) {
-			res[i][j] = 0;
-		}
-	}
+	sum = 0.;
+	// memset(res, 0.0, sizeof res);
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			res[i][j] = 0.;
 }
 void printMatrix(int matrix[N][N], string message) {
 	printf("%s\n", message.c_str());
@@ -49,6 +51,28 @@ void transposeWay() {
 			sum += res[i][j];
 		}
 } 
+void sixNestedLoop() {
+	resetMatrix();
+	double *rres, *rmul1, *rmul2;
+	int i2, k2, j2;
+	for (i = 0; i < N; i += SM) {
+		for (j = 0; j < N; j += SM) {
+			for (k = 0; k < N; k += SM) {
+				for (i2 = 0, rres = &res[i][j], 
+					rmul1 = &mul1[i][k]; i2 < SM; 
+					i2++, rres += N, rmul1 += N) {
+					for (k2 = 0, rmul2 = &mul2[k][j]; k2 < SM; k2++, rmul2 += N) {
+						for (j2 = 0; j2 < SM; j2++) {
+							rres[j2] += rmul1[k2] * rmul2[j2];
+							sum += rmul1[k2] * rmul2[j2];
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
 void performOne() {
 	sum = 0;
 	for (i = 0; i < N; ++i)
@@ -83,24 +107,29 @@ int main() {
 	// printMatrix(mul1);
 	// printMatrix(mul2);
 
-	// clock_t t = clock();
-	// basicWay();
-	// printf("basicWay time: %.3f\n", (float)(clock() - t) / CLOCKS_PER_SEC);
-	// printf("sum is: %d\n", sum);
-	// t = clock();
-	// transposeWay();
-	// printf("transposeWay time: %.4f\n", (float)(clock() - t) / CLOCKS_PER_SEC);
-	// printf("sum is: %d\n", sum);
-	
-	// perform one and two
 	clock_t t = clock();
-	performOne();
-	printf("performOne time: %.2f\n", (float)(clock() - t) / CLOCKS_PER_SEC);
-	printf("SUM: %d\n", sum);
+	basicWay();
+	printf("basicWay time: %.3f\n", (double)(clock() - t) / CLOCKS_PER_SEC);
+	printf("sum is: %.2f\n", sum);
 	t = clock();
-	performTwo();
-	printf("performOne time: %.2f\n", (float)(clock() - t) / CLOCKS_PER_SEC);
-	printf("SUM: %d\n", sum);
+	transposeWay();
+	printf("transposeWay time: %.4f\n", (double)(clock() - t) / CLOCKS_PER_SEC);
+	printf("sum is: %.2f\n", sum);
+	t = clock();
+	sixNestedLoop();
+	printf("sixNestedLoop time: %.2f\n", (double)(clock() - t) / CLOCKS_PER_SEC);
+	printf("sum: %.2f\n", sum);
+	
+	
+	// clock_t t = clock();
+	// performOne();
+	// printf("performOne time: %.2f\n", (float)(clock() - t) / CLOCKS_PER_SEC);
+	// printf("SUM: %d\n", sum);
+	// t = clock();
+	// performTwo();
+	// printf("performTwo time: %.2f\n", (float)(clock() - t) / CLOCKS_PER_SEC);
+	// printf("SUM: %d\n", sum);
+	
 
 	
 	// for (i = 0; i < N; i++) {
